90d0ce7a43e00d4f196d299d8b809f9ec01b6819
Notbell1 <abbelkadafi@gmail.com>
2026-02-10 21:33:25 +0700

    fix: use secure and stable vercel config

--- api/index.php
+++ api/index.php
@@ -1,12 +1,3 @@
 <?php
-// Pastikan autoload dan app ada
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Jalankan aplikasi
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
