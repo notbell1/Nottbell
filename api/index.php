0d39485c5f4c4a691a8f5da81cadc73291452f6d
Notbell1 <abbelkadafi@gmail.com>
2026-02-10 21:34:52 +0700

    fix: point api index to bootstrap instead of public index

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// 1. Load Composer Autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Start Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 3. Handle Request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+// 4. Kirim Response ke Browser
+$response->send();
+$kernel->terminate($request, $response);
